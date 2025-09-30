71bd014cfd497f6e9ba33144e399763842055a08
Nicolas Silva <nicolasoliveirasilva98@gmail.com>
2025-09-29 21:14:35 -0300

    Refactor OTLP export configuration in application properties and update environment variable names in Kubernetes deployment for consistency and clarity.

--- terraform/kubernetes-ms-notificacao.tf
+++ terraform/kubernetes-ms-notificacao.tf
@@ -72,11 +72,16 @@ resource "kubernetes_deployment" "deployment-ms-notificacao" {
 
           # Configurações de Observabilidade
           env {
-            name  = "OTEL_EXPORTER_OTLP_METRICS_ENDPOINT"
+            name  = "MANAGEMENT_METRICS_EXPORT_OTLP_ENDPOINT"
             value = "http://otel-collector.observability:4318/v1/metrics"
           }
+
           env {
-            name  = "OTEL_EXPORTER_OTLP_METRICS_ENABLED"
+            name  = "MANAGEMENT_METRICS_EXPORT_OTLP_PROTOCOL"
+            value = "http/protobuf"
+          }
+          env {
+            name  = "MANAGEMENT_METRICS_EXPORT_OTLP_ENABLED"
             value = "true"
           }
           env {
@@ -86,10 +91,6 @@ resource "kubernetes_deployment" "deployment-ms-notificacao" {
           env {
             name  = "MANAGEMENT_METRICS_TAGS_SERVICE"
             value = "ms-notificacao"
-          }
-          env {
-            name  = "MANAGEMENT_ENDPOINTS_WEB_EXPOSURE_INCLUDE"
-            value = "health,info,metrics,prometheus"
           }
 
           port {
