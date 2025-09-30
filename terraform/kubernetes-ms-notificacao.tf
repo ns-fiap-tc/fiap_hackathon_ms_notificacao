99808c86d7960c19ea928435b1dc4d86f0cd196e
Nicolas Silva <nicolasoliveirasilva98@gmail.com>
2025-09-30 20:30:53 -0300

    Update OTLP export configuration in application properties to use environment variables and add application-specific tags in Kubernetes deployment for enhanced observability.

--- terraform/kubernetes-ms-notificacao.tf
+++ terraform/kubernetes-ms-notificacao.tf
@@ -70,6 +70,27 @@ resource "kubernetes_deployment" "deployment-ms-notificacao" {
           #   }
           # }
 
+          env {
+            name  = "MANAGEMENT_METRICS_EXPORT_OTLP_ENDPOINT"
+            value = "http://otel-collector.observability.svc.cluster.local:4318/v1/metrics"
+          }
+          env {
+            name  = "MANAGEMENT_METRICS_EXPORT_OTLP_PROTOCOL"
+            value = "http/protobuf"
+          }
+          env {
+            name  = "MANAGEMENT_METRICS_EXPORT_OTLP_ENABLED"
+            value = "true"
+          }
+          env {
+            name  = "MANAGEMENT_METRICS_TAGS_APPLICATION"
+            value = "notificacao-service"
+          }
+          env {
+            name  = "MANAGEMENT_METRICS_TAGS_SERVICE"
+            value = "ms-notificacao"
+          }
+
           port {
             container_port = "8080"
           }
